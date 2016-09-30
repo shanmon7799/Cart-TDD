2fb9edf0d39a63d877eba871dc6ab25514793c22
shanmon7799 <shanmon7799@hotmail.com>
2016-09-30 17:05:40 +0800

    can be take out correct item

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -23,5 +23,15 @@ RSpec.describe Cart, type: :model do
   		expect(cart.items.first.quantity).to be 5
   		expect(cart.items.last.quantity).to be 3
   	end
+
+  	it "可取出正確商品" do
+  		cart = Cart.new
+  		p1 = Product.create
+  		p2 = Product.create
+  		cart.add_item(p1.id)
+  		cart.add_item(p2.id)
+  		expect(cart.items.first.product).to be_a Product
+  		expect(cart.items.last.product).to be_a Product
+  	end
   end
 end

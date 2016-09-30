b8b92ceb7b2927947d8c72c155e6f165a77e9d01
shanmon7799 <shanmon7799@hotmail.com>
2016-09-30 15:08:40 +0800

    add cart_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,18 @@
 class Cart
+	attr_reader :items
+
   def initialize
   	@items = []
   end
 
-  def add_item(product)
-  	@items << product
+  def add_item(product_id)
+  	item = @items.find { |x| x.product_id == product_id }
+
+  	if item
+  		item.increment
+  	else
+  		@items << CartItem.new(product_id)
+  	end
   end
 
   def empty?

9f7c08f767f08f339be323766ee667e0d9891b2e
shanmon7799 <shanmon7799@hotmail.com>
2016-10-04 17:55:21 +0800

    add order and order item model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 		end
   end
 
-  resources :cart, only: [:show, :destroy]
+  resource :cart, only: [:show, :destroy] do
+  	collection do
+  		get :checkout
+  	end
+  end
 
   root "products#index"
 end
